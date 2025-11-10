324e361cc63da35701de9a37859b7ad9670a5e69
Hua XU <hxu401@connect.hkust-gz.edu.cn>
2025-11-10 03:00:40 +0000

    Updated env files

---
+++ requirements_other.sh
@@ -0,0 +1,15 @@
+# conda install pytorch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+
+# conda install -c dglteam/label/th24_cu118 dgl
+
+wget https://download.pytorch.org/whl/cpu/torchdata-0.9.0%2Bcpu-cp39-cp39-linux_x86_64.whl#sha256=5656b119575c067fd48d4b52716666fb55f7bd643164c785124e67b23f376f94
+
+wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/pyg_lib-0.4.0%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
+
+wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/torch_cluster-1.6.3%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
+
+wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/torch_scatter-2.1.2%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
+
+wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/torch_sparse-0.6.18%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
+
+wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/torch_spline_conv-1.2.2%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
