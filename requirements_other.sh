e46f6bfb9a99db6f123feff6cff671bd25ceb02c
Hua XU <hxu401@connect.hkust-gz.edu.cn>
2025-11-12 05:18:03 +0800

    Update environment

--- requirements_other.sh
+++ requirements_other.sh
@@ -1,7 +1,5 @@
 pip install torch==2.3.1 --index-url https://download.pytorch.org/whl/cu118
 
-# conda install -c dglteam/label/th24_cu118 dgl
-
 wget https://download.pytorch.org/whl/cpu/torchdata-0.9.0%2Bcpu-cp39-cp39-linux_x86_64.whl#sha256=5656b119575c067fd48d4b52716666fb55f7bd643164c785124e67b23f376f94
 
 wget https://data.pyg.org/whl/torch-2.3.0%2Bcu118/pyg_lib-0.4.0%2Bpt23cu118-cp39-cp39-linux_x86_64.whl
